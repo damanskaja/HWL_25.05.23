2085de680c252a19a99d4d36c2b3a670913ec789
Yuliia Damanska <yuliiadamanska@Yuliias-Air.fritz.box>
2023-05-29 16:31:01 +0200

    task5

--- sleeper.sh
+++ sleeper.sh
@@ -11,3 +11,8 @@ cat /proc/cpuinfo > processor_info.txt
 cat /etc/os-release | grep "NAME=" > os_info.txt
 
 cat /etc/os-release | grep "NAME=" | awk -F= '{print $2}' | awk -F\" '{print $2}' > os_name.txt
+
+for ((i=50; i<=100; i++))
+do
+    touch "${i}.txt"
+done
